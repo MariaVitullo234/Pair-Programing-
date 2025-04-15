1e72ddf516566661d85adace3484201b67860eaa
Maria Vitullo <mvitullo@uiowa.edu>
2025-04-15 15:02:02 -0500

    Added code for website

---
+++ App.py
@@ -0,0 +1,14 @@
+# Maria and Emma 
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')  # Render the index.html template when accessing the root URL
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002, debug=True)
+    
